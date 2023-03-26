282ecce90a3c95fd69f3ea903466757d383876a8
Jan Steikowski <j.steikowski@aol.com>
2023-03-26 13:11:21 +0700

    Start the form check in javascript and comment the code

--- browser.js
+++ browser.js
@@ -1,5 +1,7 @@
+// initialize library array
 const myLibrary = [];
 
+// initialize book object
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -10,12 +12,14 @@ function Book(title, author, pages, read) {
   };
 }
 
+// function to add a book to the library
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-
+// container in which the bookDivs are stored
 const bookContainer = document.getElementById('bookContainer');
 
+// function to display the books in the book Container
 function displayBooks() {
   myLibrary.forEach((book) => {
     const bookDiv = document.createElement('div');
@@ -33,43 +37,79 @@ function displayBooks() {
     bookDiv.classList.add('bookDiv');
   });
 }
-
+// form to add a book
 const form = document.getElementById('addBookForm');
+// button to open the form
 const addButton = document.getElementById('addBook');
+// button to submit the form
 const finishForm = document.getElementById('finishForm');
-
+// div to store the button which opens the form
 const addDelete = document.getElementById('addDelete');
 
+// function to blurr everything except the form
 function makeAllBlurred() {
   bookContainer.classList = ['blurred'];
   addDelete.classList = ['blurred'];
 }
 
+// function to unblurr everything
 function makeAllUnBlurred() {
   bookContainer.classList = [''];
   addDelete.classList = [''];
 }
 
+// function to make the form visible
 function makeFormVisible() {
   makeAllBlurred();
   addButton.disabled = true;
   form.classList = ['visible'];
 }
 
+// function to clear the bookContainer div in order to add all new divs again
 function clearContainer() {
   const allBookDivs = Array.from(document.getElementsByClassName('bookDiv'));
   allBookDivs.forEach((book) => { book.remove(); });
 }
 
+// function to check if the pages input contains only numbers or nothing
+function checkPages() {
+  if (form.bookPages.value === '') {
+    form.bookPages.value = 'unknown';
+    return true;
+  }
+  if (form.bookPages.value.match(/^\d+$/)) {
+    return true;
+  }
+  return false;
+}
+
+// function to check the form before submitting
+function checkForm() {
+  if (!checkPages()) {
+    return false;
+  }
+  return true;
+}
+
+// function to surpress the form sending and call the necessary forms to add the new book
 function dontSendForm(event) {
-  // eslint-disable-next-line max-len
-  addBookToLibrary(new Book(form.bookName.value, form.bookAuthor.value, form.bookPages.value, form.readCheck.value));
-  clearContainer();
-  displayBooks();
-  makeAllUnBlurred();
-  form.classList = ['invis'];
+  if (checkForm()) {
+    // eslint-disable-next-line max-len
+    addBookToLibrary(
+      new Book(
+        form.bookName.value,
+        form.bookAuthor.value,
+        form.bookPages.value,
+        form.readCheck.value,
+      ),
+    );
+    clearContainer();
+    displayBooks();
+    makeAllUnBlurred();
+    form.classList = ['invis'];
+    addButton.disabled = false;
+  }
   event.preventDefault();
-  addButton.disabled = false;
 }
 
 addButton.addEventListener('click', makeFormVisible);
